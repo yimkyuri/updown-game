7172302647c3509188d6a81850e8a365062dce32
kyuri <yimkyuri@naver.com>
2024-02-12 01:24:20 +0900

    second commit

--- main.js
+++ main.js
@@ -1,34 +1,38 @@
 let pcNumber = 0;
 let userInput = document.getElementById("user-input");
 let playButton = document.getElementById("play-button");
-let resultArea = document.getElementById("result-area");
+let notice = document.getElementById("notice");
 let resetButton = document.getElementById("reset-button");
 let chances = 5;
 let chancesArea = document.getElementById("chances-area");
 let gameOver = false;
 let history = [];
+let visualImg = document.getElementById("visual-img");
 
 function play() {
     let userValue = userInput.value;
     if ( userValue < 1  || userValue > 100 || userValue == '' ) {
-        resultArea.textContent = " 1~100 사이의 숫자를 입력해주세요."
+        notice.textContent = "[ ! ] 1~100 사이의 숫자를 입력해주세요."
         return
     }
     if ( history.includes(userValue) ) {
-        resultArea.textContent = " 이미 사용한 숫자 입니다."
+        notice.textContent = "[ ! ] 이미 사용한 숫자 입니다."
         return
-    }
+    } 
 
     chances --;
-    chancesArea.textContent = `${chances}번의 기회가 남았습니다.`
+    chancesArea.textContent = chances
     console.log("Chance", chances);
 
     if ( userValue > pcNumber ) {
-        resultArea.textContent = "Down!"
+        // notice.textContent = "Down!"
+        visualImg.src = "img/img3.jpg";
     } else if ( userValue < pcNumber ) {
-        resultArea.textContent = "Up!"
+        // notice.textContent = "Up!"
+        visualImg.src = "img/img2.jpg";
     } else {
-        resultArea.textContent = "정답입니다!"
+        // notice.textContent = "정답입니다!"
+        visualImg.src = "img/img4.jpg";
         gameOver = true;
     }
     history.push(userValue);
@@ -40,24 +44,28 @@ function play() {
 
     if ( gameOver == true ) {
         playButton.disabled =  true;
+        if ( userValue != pcNumber ) {
+            visualImg.src = "img/img5.jpg";
+        }
     }
 
 }
 
 function reset() {
     userInput.value = "";
-    resultArea.textContent = "결과가 이곳에 나옵니다."
+    visualImg.src = "img/img1.jpg";
     playButton.disabled =  false;
     randomNumber();
     history = [];
     gameOver = false;
+    notice.textContent = ""
 }
 
 function randomNumber() {
     pcNumber = Math.floor( Math.random() * 100 ) + 1;
     console.log ("정답", pcNumber);
     chances = 5;
-    chancesArea.textContent = `${chances}번의 기회가 남았습니다.`
+    chancesArea.textContent = chances
 }
 
 
